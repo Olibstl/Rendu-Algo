602b5847a053dfba9e93b6fe08a49b4f7fd12ae8
olibstl <olivia.bastel@edu.devinci.fr>
2022-11-09 16:31:31 +0100

    Projet

---
+++ main.js
@@ -0,0 +1,75 @@
+let person = {
+    name: 'John',
+    Cool: 10,
+}
+
+const musics = [
+    "Wejdene - Anissa",
+    "Keen'v - Rien qu'une fois",
+    "mM. Pokora - À nos actes manqués",
+    "L'artiste - Mon pays",
+    "Amel Bant - Viser la lune",
+]
+
+let trajet = {
+    changes: 0,
+    radio: musics[0],
+    redlights: 30,
+}
+
+let random=Math.trajet(Math.random(4, 0)* 0)
+
+function takeTaxi(person, trajet){
+
+    while(trajet.redlights > 0){
+        console.log(person.Name + 'reste dans le taxi')
+
+        if(musics[random] == musics[0]){
+            person.cool--
+            trajet.changes++
+        }
+            trajet.redlights--
+    }
+
+
+
+
+
+
+
+
+    let skiller = {
+        name: "Jason",
+        life: 100,
+    }
+
+    const caracteristics = [
+        "nerd",
+        "méchant",
+        "insolant",
+        "Sportif",
+        "Bavard",
+    ]
+
+    
+    
+
+    const name =[
+
+    ]
+
+
+    class survivants (
+        constructor("Sarah", "Johanna", "Patrick", "Correy", "Lionnel"){
+            this.Sarah
+            this.Johanna
+            this.Patrick
+            this.Correy
+            this.Lionnel
+        }
+
+    let survivor1 = new survivor
+    )
+
+    
+
